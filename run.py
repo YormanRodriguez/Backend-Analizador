765d4163fda2913f8e01a03cd4e9c863d8ec1992
Yorman Rodolfo Rodriguez Jaimes <168009922+YormanRodriguez@users.noreply.github.com>
2025-06-19 12:08:29 -0500

    Agregar el parámetro de host en la ejecución de uvicorn

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("Analizador.main:app", port=8000, reload=True)
+    uvicorn.run("Analizador.main:app", host="0.0.0.0", port=8000, reload=True)
